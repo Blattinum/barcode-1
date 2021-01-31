e38ec4775679e83b01b2c26519c8c7e6cf127382
Blattinum <collin.blatt@gmail.com>
2021-01-31 13:28:21 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,5 @@
 document.querySelectorAll('.barcode').insertAdjacentHTML('afterbegin', '*');
 document.querySelectorAll('.barcode').insertAdjacentHTML('beforeend', '*');
+
+
+console.log('barcodes r kewl');
